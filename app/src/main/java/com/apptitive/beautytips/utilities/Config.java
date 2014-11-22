e741fa6e498e41de44e8895ea8395442fdeda95e
iftekhar-ahmed <bit0220iftekhar@gmail.com>
2014-11-22 21:00:25 +0600

    Change MENU_URL and BASE_IMAGE_URL in Config.java. Change color themes.

--- app/src/main/java/com/apptitive/beautytips/utilities/Config.java
+++ app/src/main/java/com/apptitive/beautytips/utilities/Config.java
@@ -8,7 +8,7 @@ import android.content.Context;
 public class Config {
     private static final String MENU_URL = "http://apptitive.com/content_display/json/menu.json";
     private static final String TOPIC_URL = "http://apptitive.com/content_display/json/content.json";
-    private static final String BASE_IMAGE_URL="http://apptitive.com/content_display/media/images/";
+    private static final String BASE_IMAGE_URL="http://apptitive.com/beauty_tips/media/images/";
 
     public static String getMenuUrl() {
         return MENU_URL;
